43361f87425c2a1d08826d5f1ac6d1bec4a4f3c1
Stephane Bortzmeyer <bortzmeyer@nic.fr>
2012-09-23 17:31:53 +0200

    Support several files as input

--- DNSmezzo/reports/kiminsky.py
+++ DNSmezzo/reports/kiminsky.py
@@ -18,47 +18,47 @@ def by_ratio (l, r):
     else:
         return 0
 
-if len(sys.argv) == 1:
-    raise Exception("Usage: %s file-number" % sys.argv[0])
-# TODO: allow several files
-fileno = int(sys.argv[1])
+if len(sys.argv) <= 1:
+    raise Exception("Usage: %s file-number ..." % sys.argv[0])
+for arg in sys.argv[1:]:
+    fileno = int(arg)
 
-conn = psycopg2.connect(dbinfo)
-cursor = conn.cursor()
-if fileno == 0:
-    files = ""
-else:
-    files = "AND file=%i" % fileno
-cursor.execute("""
-SELECT src_address AS resolver, count(id) AS requests, count(DISTINCT src_port) AS ports 
-   FROM DNS_Packets WHERE query AND NOT rd %s
-         GROUP BY resolver ORDER BY requests;
-""" % files)
-clients = {}
-cl_ports = {}
-cl_requests = {}
-num_vulnerable = 0
-requests_vulnerable = 0
-num_kept = 0
-requests_kept = 0
-for client in cursor.fetchall():
-    requests = int(client[1])
-    ports = int(client[2])
-    if requests < MIN_REQUESTS:
-        continue
-    num_kept += 1
-    requests_kept += requests
-    if requests > (65536-1024): # Maximum number of ports one can reasonably use
-        capped_requests = 65536-1024
+    conn = psycopg2.connect(dbinfo)
+    cursor = conn.cursor()
+    if fileno == 0:
+        files = ""
     else:
-        capped_requests = requests
-    ratio = float(ports) / float(capped_requests) 
-    if ratio < MIN_RATIO:
-        num_vulnerable += 1
-        requests_vulnerable += requests
-        clients[client[0]] = ratio
-        cl_ports[client[0]] = ports
-        cl_requests[client[0]] = requests
+        files = "AND file=%i" % fileno
+    cursor.execute("""
+    SELECT src_address AS resolver, count(id) AS requests, count(DISTINCT src_port) AS ports 
+       FROM DNS_Packets WHERE query AND NOT rd %s
+             GROUP BY resolver ORDER BY requests;
+    """ % files)
+    clients = {}
+    cl_ports = {}
+    cl_requests = {}
+    num_vulnerable = 0
+    requests_vulnerable = 0
+    num_kept = 0
+    requests_kept = 0
+    for client in cursor.fetchall():
+        requests = int(client[1])
+        ports = int(client[2])
+        if requests < MIN_REQUESTS:
+            continue
+        num_kept += 1
+        requests_kept += requests
+        if requests > (65536-1024): # Maximum number of ports one can reasonably use
+            capped_requests = 65536-1024
+        else:
+            capped_requests = requests
+        ratio = float(ports) / float(capped_requests) 
+        if ratio < MIN_RATIO:
+            num_vulnerable += 1
+            requests_vulnerable += requests
+            clients[client[0]] = ratio
+            cl_ports[client[0]] = ports
+            cl_requests[client[0]] = requests
 sorted_clients = clients.keys()
 sorted_clients.sort(by_ratio)
 print """%2.2f %% of clients with more than %i requests are vulnerable to the Kaminsky attack 
