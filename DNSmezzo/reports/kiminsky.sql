711350186e5269e01cbb72de6ecf6f1d8fa83759
Xavier Beaudouin <xbeaudouin@gmail.com>
2012-11-06 15:16:52 +0100

    Updated Readme requirement about gmake Added an another way to get some kiminisky stats

--- DNSmezzo/reports/kiminsky.sql
+++ DNSmezzo/reports/kiminsky.sql
@@ -4,3 +4,9 @@
 SELECT src_address AS resolver,count(id) AS requests, count(DISTINCT src_port) AS ports FROM DNS_Packets WHERE file=55
          GROUP BY resolver 
          ORDER BY ports;
+
+-- Another way to make like kiminsky.py using PGSQL 
+SELECT src_address AS resolver, count(id) AS requests, count(DISTINCT src_port) AS ports FROM dns_packets WHERE file=55 AND query 
+        GROUP BY resolver 
+        HAVING count(id) >= 10 
+        ORDER BY requests
